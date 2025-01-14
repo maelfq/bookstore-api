707b038134192b017a5622dca5ecfe0e1adf6a48
Mael F <mael.fq44@gmail.com>
2025-01-14 06:00:22 +0100

    feature: java-api
    get available books - wip

--- bookstore-api-java/app/src/main/java/bookstore/controller/RentBookController.java
+++ bookstore-api-java/app/src/main/java/bookstore/controller/RentBookController.java
@@ -1,14 +1,30 @@
 package bookstore.controller;
 
+import bookstore.database_entity.PhysicalBookEntity;
+import bookstore.dto.BookDto;
 import bookstore.dto.PhysicalBookDto;
+import bookstore.repository.PhysicalBookRepository;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rent-book")
 public class RentBookController {
 
-    public PhysicalBookDto rentBookController(PhysicalBookDto physicalBookDto) {
+    private final PhysicalBookRepository physicalBookRepository;
+
+    public RentBookController(PhysicalBookRepository physicalBookRepository) {
+        this.physicalBookRepository = physicalBookRepository;
+    }
+
+    @PostMapping
+    public PhysicalBookDto rentBookController(BookDto bookDto) {
+        List<PhysicalBookEntity> physicalBooks = physicalBookRepository.findByBookEntity_BookId(bookDto.getBookId());
+        // TODO: get available books
+        physicalBooks.stream().filter(physicalBookEntity -> physicalBookEntity.getCustomerEntity() != null).toList();
         return null;
     }
 }
--- bookstore-api-java/app/src/main/java/bookstore/repository/PhysicalBookRepository.java
+++ bookstore-api-java/app/src/main/java/bookstore/repository/PhysicalBookRepository.java
@@ -4,6 +4,9 @@ import bookstore.database_entity.PhysicalBookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhysicalBookRepository extends JpaRepository<PhysicalBookEntity, Long> {
-}
+    List<PhysicalBookEntity> findByBookEntity_BookId(Long bookId);
+ }
